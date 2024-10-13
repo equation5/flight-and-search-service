2648303e37b248138e5186ccd3363b668a32da0f
Aditya Kumar <kumar00adi@gmail.com>
2024-10-13 17:36:22 +0530

    setup basic env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-hi
+// Importing the required modules
+const express = require('express');
+const {PORT}=require('./config/serverconfig');  
+const setupandstartserver=async ()=>
+{
+// Initializing the app
+const app = express();
+// Defining a simple route
+// Listening on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    
+});
+}
+setupandstartserver();
