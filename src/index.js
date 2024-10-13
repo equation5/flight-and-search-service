6d839a13633fc80bcc1f0d137e676dea7314454a
Aditya Kumar <kumar00adi@gmail.com>
2024-10-13 21:15:36 +0530

    setup basic bodyparser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 // Importing the required modules
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT}=require('./config/serverconfig');  
 const setupandstartserver=async ()=>
 {
 // Initializing the app
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// Parse application/json
+app.use(bodyParser.json());
 // Defining a simple route
 // Listening on port 3000
 const PORT = 3000;
